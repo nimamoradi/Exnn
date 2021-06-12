8d4c7c8d29f4e7aac1ffa14a3b6f949d08d7a29d
Nima Moradi <ni_moradi@yahoo.com>
2021-06-12 09:20:40 +0430

    Update gamnet.py

--- exnn/gamnet.py
+++ exnn/gamnet.py
@@ -3,71 +3,15 @@ from .base import BaseNet
 
 
 class GAMNet(BaseNet):
-    """
-    Generalized additive model vai neural network implementation. It is just a simplified version of exnn with identity projection layer.
+ 
 
-    Parameters
-    ----------
-    :type  meta_info: dict
-    :param meta_info: the meta information of the dataset.
-
-    :type  subnet_arch: list
-    :param subnet_arch: optional, default=(10, 6).
-        The architecture of each subnetworks, the ith element represents the number of neurons in the ith layer.
-
-    :type  task_type: string
-    :param task_type: optional, one of {"Regression", "Classification"}, default="Regression". Only support binary classification at current version.
-
-    :type  batch_size: int
-    :param batch_size: optional, default=1000, size of minibatches for stochastic optimizers.
-
-    :type  training_epochs: int
-    :param training_epochs: optional, default=10000, maximum number of training epochs.
-
-    :type  activation: tf object
-    :param activation: optional, default=tf.tanh.
-        Activation function for the hidden layer of subnetworks. It can be any tensorflow activation function object.
-
-    :type  lr_bp: float
-    :param lr_bp: optional, default=0.001, learning rate for weight updates.
-
-    :type  beta_threshold: float
-    :param beta_threshold: optional, default=0.01.
-        Percentage threshold for pruning the subnetworks, which means the subnetworks that sum up to 95% of the total sclae will be kept.
-
-    :type  tuning_epochs: int
-    :param tuning_epochs: optional, default=500, the number of tunning epochs.
-
-    :type  l1_proj: float
-    :param l1_proj: optional, default=0.001, the strength of L1 penalty for projection layer.
-
-    :type  l1_subnet: float
-    :param l1_subnet: optional, default=0.001, the strength of L1 penalty for scaling layer.
-
-    :type  l2_smooth: float
-    :param l2_smooth: optional, default=0.000001, the strength of roughness penalty for subnetworks.
-
-    :type  verbose: bool
-    :param verbose: optional, default=False. If True, detailed messages will be printed.
-
-    :type  val_ratio: float
-    :param val_ratio: optional, default=0.2. The proportion of training data to set aside as validation set for early stopping. Must be between 0 and 1.
-
-    :type  early_stop_thres: int
-    :param early_stop_thres: optional, default=1000. Maximum number of epochs if no improvement occurs.
-
-    :type  random_state: int
-    :param random_state: optional, default=0, the random seed.
-
-    """
-
-    def __init__(self, meta_info, subnet_arch=[10, 6], task_type="Regression",
+    def __init__(self, subnet_arch=[10, 6], task_type="Regression",
                  activation_func=tf.tanh, batch_size=1000, training_epochs=10000, lr_bp=0.001,
                  beta_threshold=0.05, tuning_epochs=500, l1_subnet=0.001, l2_smooth=0.000001,
                  verbose=False, val_ratio=0.2, early_stop_thres=1000):
 
-        super(GAMNet, self).__init__(meta_info=meta_info,
-                             subnet_num=len(meta_info) - 1,
+        super(GAMNet, self).__init__(
+                             subnet_num=1,
                              subnet_arch=subnet_arch,
                              task_type=task_type,
                              proj_method="gam",
